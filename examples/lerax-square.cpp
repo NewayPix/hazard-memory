fb27d2059bc030f3c49743bbd3dab86c46be18c3
Manoel Vilela <manoel_vilela@engineer.com>
2020-08-01 23:03:49 -0300

    Improves examples/lerax-square.cpp (#7)
    * Initialize structs always: non-initialized fields undefined behavior
    
    * Remove useless comments
    
    * Use automatic storage duration instead dynamic
    
    That's ensure that the destructor it's called in the end of scope.
    Using new to allocate we need to explicitly call the destructor.
    
    * Set namespace to std to simplify verbosity
    
    * Use Vector2D implementation on lerax-square.cpp

--- examples/lerax-square.cpp
+++ examples/lerax-square.cpp
@@ -3,9 +3,12 @@
 #include <string>
 
 #include <SDL2/SDL.h>
+#include <Vector2D.hpp>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+using namespace std;
 
 struct KeyboardState {
     bool up;
@@ -21,57 +24,42 @@ struct KeyboardState {
 };
 
 struct Player {
-    float x;
-    float y;
-    float d_x; /* direction x: 1 = right; -1 = left */
-    float d_y; /* direction y: 1 = up; -1 = down */
+    Vector2D position = {0, 0};
+    Vector2D direction = {0, 0};
     int size = 50;
     SDL_Rect rect = {0, 0, size, size};
     float velocity = 300;
 
     void set_direction(struct KeyboardState *k) {
-        d_x = k->right? 1: k->left? -1: 0;
-        d_y = k->up? 1: k->down? -1: 0;
+        direction.x = k->right? 1: k->left? -1: 0;
+        direction.y = k->up? 1: k->down? -1: 0;
     }
 
     float cos_direction() {
-        float u_x = 1;
-        float u_y = 0;
-        float dot_u_v = (u_x *  d_x) + (u_y * d_y);
-        float u_norm = sqrt(u_x * u_x + u_y * u_y);
-        float v_norm = sqrt(d_x * d_x + d_y * d_y);
-
-        return dot_u_v / (u_norm * v_norm);
+        Vector2D unit_vector(1, 0);
+        return direction.cos(unit_vector);
     }
 
     float sin_direction() {
         float cos = cos_direction();
         float sin = sqrt(1 - cos*cos);
-        return -d_y * sin;
+        return -direction.y * sin;
     }
 
     void move(float dt) {
-        // std::cout << "cos: " << cos << " sin: " << sin << std::endl;
-        x += cos_direction() * velocity * dt;
-        y += sin_direction() * velocity * dt;
+        position.x += cos_direction() * velocity * dt;
+        position.y += sin_direction() * velocity * dt;
     }
 };
 
 class Game {
 private:
-    //The window we'll be rendering to
     SDL_Window *window = NULL;
-
-    // Renderer object
     SDL_Renderer *renderer = NULL;
-
-    // player
-    struct Player player;
     const char *title = "Square Moving";
-
     bool running = true;
-
-    struct KeyboardState keyboard;
+    struct Player player;
+    struct KeyboardState keyboard = {};
 
     void event(SDL_Event *e) {
         while (SDL_PollEvent(e)) {
@@ -140,19 +128,19 @@ private:
             player.move(dt);
         }
 
-        if (player.x < 0) {
-            player.x = 0;
-        } else if (player.x + player.size > SCREEN_WIDTH) {
-            player.x = SCREEN_WIDTH - player.size;
+        if (player.position.x < 0) {
+            player.position.x = 0;
+        } else if (player.position.x + player.size > SCREEN_WIDTH) {
+            player.position.x = SCREEN_WIDTH - player.size;
         }
-        if (player.y < 0) {
-            player.y = 0;
-        } else if (player.y + player.size > SCREEN_HEIGHT) {
-            player.y = SCREEN_HEIGHT - player.size;
+        if (player.position.y < 0) {
+            player.position.y = 0;
+        } else if (player.position.y + player.size > SCREEN_HEIGHT) {
+            player.position.y = SCREEN_HEIGHT - player.size;
         }
 
-        player.rect.x = round(player.x);
-        player.rect.y = round(player.y);
+        player.rect.x = round(player.position.x);
+        player.rect.y = round(player.position.y);
     }
 
     void render() {
@@ -170,17 +158,14 @@ private:
 
 public:
     Game() {
-        std::cout << ":: Game initialization!" << std::endl;
-        // player position initialization
-        player.x = SCREEN_WIDTH / 2;
-        player.y = SCREEN_HEIGHT / 2;
+        cout << ":: Game initialization!" << endl;
+        player.position.x = SCREEN_WIDTH / 2;
+        player.position.y = SCREEN_HEIGHT / 2;
 
-        //Initialize SDL
         if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-            throw std::string("SDL could not initialize: ") + SDL_GetError();
+            throw string("SDL could not initialize: ") + SDL_GetError();
         }
         else {
-            //Create window
             window = SDL_CreateWindow(title,
                                       SDL_WINDOWPOS_UNDEFINED,
                                       SDL_WINDOWPOS_UNDEFINED,
@@ -188,12 +173,12 @@ public:
                                       SCREEN_HEIGHT,
                                       SDL_WINDOW_SHOWN);
             if(window == NULL) {
-                throw std::string("window could not be created: ") + SDL_GetError();
+                throw string("window could not be created: ") + SDL_GetError();
             }
             else {
                 renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
                 if (!renderer) {
-                    throw std::string("renderer could not be created: ") + SDL_GetError();
+                    throw string("renderer could not be created: ") + SDL_GetError();
                 }
 
             }
@@ -201,7 +186,7 @@ public:
     }
 
     ~Game() {
-        std::cout << ":: Game being destroyed!" << std::endl;
+        cout << ":: Game being destroyed!" << endl;
         SDL_DestroyRenderer(renderer);
         renderer = NULL;
 
@@ -229,11 +214,10 @@ public:
 
 int main(void) {
     try {
-        Game *g = new Game();
-        g->run();
-        delete g;
-    } catch (std::string s) {
-        std::cerr << "[error] " << s << std::endl;
+        Game game;
+        game.run();
+    } catch (string s) {
+        cerr << "[error] " << s << endl;
         return 1;
     }
 
