5a6c01e1297a4f13c94b418639a7422ffa151bc7
Rafael C. Nunes <rcamposnunes@outlook.com>
2020-08-04 09:02:06 -0300

    Reinforce good C++ practices (#11)
    * Change pointers NULL to nullptr
    
        The NULL is not statically typed and does not provide the safety
    that C++ brings with C++11 when comparing pointers. Thus, the change was
    done to improve code in this aspect.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>
    
    * Change <> for "", meaning its not a system file
    
        In C or C++ its common to use "" instead of <> to indicate to the
    compiler that the file is availabble on a local folder within the code
    being compiled and it should look at that first instead of the default
    system include headers directory
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- examples/lerax-square.cpp
+++ examples/lerax-square.cpp
@@ -3,7 +3,8 @@
 #include <string>
 
 #include <SDL2/SDL.h>
-#include <Vector2D.hpp>
+
+#include "Vector2D.hpp"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -54,8 +55,8 @@ struct Player {
 
 class Game {
 private:
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
     const char *title = "Square Moving";
     bool running = true;
     struct Player player;
@@ -172,7 +173,7 @@ public:
                                       SCREEN_WIDTH,
                                       SCREEN_HEIGHT,
                                       SDL_WINDOW_SHOWN);
-            if(window == NULL) {
+            if(window == nullptr) {
                 throw string("window could not be created: ") + SDL_GetError();
             }
             else {
@@ -188,10 +189,10 @@ public:
     ~Game() {
         cout << ":: Game being destroyed!" << endl;
         SDL_DestroyRenderer(renderer);
-        renderer = NULL;
+        renderer = nullptr;
 
         SDL_DestroyWindow(window);
-        window = NULL;
+        window = nullptr;
 
         SDL_Quit();
     }
