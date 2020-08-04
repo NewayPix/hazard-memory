897d2152f675a37bb940ed6e8f39835f8aa41365
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-08-04 11:28:55 -0300

    Update colliders functions and add abstractions
        The rect collider has two more functions that abstract the bounds
    and dimension of the collider. Other modifications, regarding the
    positioning of public variables inside classes is adjusted.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/collider/ColliderRect.cpp
+++ src/collider/ColliderRect.cpp
@@ -1,6 +1,26 @@
 #include "ColliderRect.hpp"
 
-ColliderRect::ColliderRect(SDL_Rect *r): Collider(r) {}
-ColliderRect::ColliderRect(int x, int y, int w, int h): Collider(x, y, w, h) {}
+ColliderRect::ColliderRect(SDL_Rect *r): Collider(r) {
+
+}
+
+ColliderRect::ColliderRect(int x, int y, int w, int h): Collider(x, y, w, h) {
+
+}
 
 ColliderRect::~ColliderRect() {}
+
+
+bool ColliderRect::on_top(const ColliderRect &c) {
+    float rect_distance = c.polygon->y - \
+                            (this->polygon->y + this->polygon->h);
+
+    int x_min = c.polygon->x - this->polygon->w;
+    int x_max = c.polygon->x + this->polygon->w;
+
+    bool cond = (this->polygon->x >= x_min && \
+                    this->polygon->x <= x_max && rect_distance == 0);
+
+    return cond;
+}
+
