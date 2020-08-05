f180bdbf822b125556946a50d2a67ce903fa9e26
Manoel Vilela <manoel_vilela@engineer.com>
2020-08-05 00:11:23 -0300

    Finally remove SDL_Rect polygon from Collider abstract class
    - Add method radius to replace need of polygon lookup
    - I know it will works for Circles!
    - Move SDL_Rect polygon from Collider to ColliderRect definition
    - Refactor ColliderRect::collide to use radius

--- src/collider/Collider.hpp
+++ src/collider/Collider.hpp
@@ -2,9 +2,6 @@
 #define COLLISION_HPP
 
 #include <cmath>
-
-#include <SDL2/SDL.h>
-
 #include "math/Vector2.hpp"
 
 class Collider {
@@ -12,13 +9,16 @@ public:
     // The center of the collider polygon, it must be calculated at the
     // constructor
     Vector2 center;
-    // The polygon is the surface on which
-    SDL_Rect polygon;
 
     /**
      * @brief abstract collide method
      */
     virtual bool collide(const Collider &c) = 0;
+
+    /**
+     * @brief return the object radius
+     */
+    virtual Vector2 radius() const = 0;
 };
 
 #endif //COLLISION_HPP
